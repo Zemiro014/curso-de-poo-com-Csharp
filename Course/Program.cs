5c7beef8e0a09c052039f0f41769882d430bf485
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-21 11:42:42 -0300

    Trabalhando com Vetores

--- Course/Program.cs
+++ Course/Program.cs
@@ -7,39 +7,23 @@ namespace Course
     {       
         static void Main(string[] args)
         {
-            /*
-              Nullable: permite fazer com que os tipos valores ( struct) aceitem valor como "null"
-             */
-            Nullable<double> x = null; // permite que um "double" aceite um valor null
-            // também pode ser representado por:
-            int? y = null; // permite que um "int" aceite um valor null
+            Console.WriteLine("Informe a quantidade de pessoas");
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
 
-            double? a = null;
-            double? b = 10.0;
-
-            // Pega o valor da variável, se não tiver pega o valor padrão desta variável
-            Console.WriteLine(a.GetValueOrDefault());
-            Console.WriteLine(b.GetValueOrDefault());
-
-            // Verifica se nas variáveis existem valores
-            Console.WriteLine(a.HasValue);
-            Console.WriteLine(b.HasValue);
-
-            // Pega o valor da variável. Resulta numa exceção se o valor da variável for "null"
-            if (a.HasValue)
-            { Console.WriteLine(a.Value); }
-            else 
+            for (int i = 0; i < n; i++) 
             {
-                Console.WriteLine(" a tem valor null");
-            }            
-            Console.WriteLine(b.Value);
+                Console.WriteLine("Digite a altura da " + (i + 1) + "ª " + "pessoa");
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
 
-            Console.WriteLine(" Operador de coalescência nula ");
-            // Operador de coalescência nula
-            double z = a ?? 5.0; // Significa que: o "z" recebe o valor de "a", mas se o valor de "a" for "null" então o "z" recebe o valor 5.0
-            double w = b ?? 2; // Significa que: o "w" recebe o valor de "b", mas se o valor de "b" for "null" então o "w" recebe o valor 2
-            Console.WriteLine(z);
-            Console.WriteLine(w);
+            double soma = 0;
+            for (int i = 0; i < vect.Length; i++)
+            {
+                soma += vect[i];
+            }
+
+            Console.WriteLine("A media das alturas é de " + (soma / vect.Length));
         }
     }
 }
