95fca538b7f08a280615fda6a8d1ea22f6120475
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-21 13:04:08 -0300

    Definindo um método que possa receber parâmetros variáveis

--- Course/Calculadora.cs
+++ Course/Calculadora.cs
@@ -17,5 +17,46 @@ namespace Course
         {
             return (4.0 / 3.0) * pi * r * r * r;
         }
+
+        // As várias formas de definir função com parâmetros variáveis
+
+        // 1º SobreCarga: Funciona, mas é muioto trabalhoso
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+        // Termina aqui a sobrecarga de função
+
+        // 2º Uma função que recebe como parãmetro um vector: É bom e funciona. Mas é chato por ter que usar "new int[] {1, 2, 3}" ao chamar ele
+        public static int Sum01(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+        
+
+        // 3º Usando o "params" em combinação com um vetor como parametro da função. É bastante prático: permite inserir quantos parâmetros tu desejar.
+                // Exemplo: chamando o sum02(1, 2, 3, 4, 5, 6, 7, 8) funcina perfeitamente
+        public static int Sum02(params int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
     }
 }
--- Course/Program.cs
+++ Course/Program.cs
@@ -4,39 +4,29 @@ using System.Globalization;
 namespace Course
 {
     class Program
-    {       
+    {
         static void Main(string[] args)
         {
-            // Trabalhando com vectores que recebem tipo class
-            Console.WriteLine("Informe a quantidade de produtos");
-            int n = int.Parse(Console.ReadLine());
-            Produto[] vect = new Produto[n];
+            // soma de 2 números
+            int s1 = Calculadora.Sum(2, 3);
 
-            for (int i = 0; i < n; i++) 
-            {
-                Console.WriteLine("Digite o nome do " + (i + 1) + "º " + "produto");
-                string nome = Console.ReadLine();
-                Console.WriteLine("Digite o preço do " + (i + 1) + "º " + "produto");
-                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                /*
-                 * Pode ser feita deste jeito abaixo: usando construtor automático
-                 * vect[i] = new Produto
-                 { Nome = nome, Preco = preco };
+            // Usar sobre carga para somar 3 numeros
+            int s2 = Calculadora.Sum(2, 3, 4);
+            Console.WriteLine("A soma de 2 + 3 é de: "+s1);
+            Console.WriteLine("A soma de 2 + 3 + 4 é de: " + s2);
+            /*
+             * Sobrecarregar métodos é muito trabalhoso. Para solucionar isso ai usa métodos que recebem "vetores". Fazendo isso podemos somar quantos números a gente quiser
+             */
+            int s3 = Calculadora.Sum01(new int[] { 2, 3, 4, 4, 7, 8 });
+            Console.WriteLine("A soma de 2 + 3 + 4 + 4 + 7 + 8 é de: " + s3);
 
-                 Ou por construtor normal.
-                */
-                vect[i] = new Produto(nome, preco);
-
-                // A regra é que cada objecto "vect[i]" precisa ser instanciada por "new Produto()" 
-            }
-
-            double soma = 0.0;
-            for (int i = 0; i < vect.Length; i++)
-            {
-                soma += vect[i].Preco;
-            }
-
-            Console.WriteLine("A media do preço dos produtos é de " + (soma / vect.Length));
+            /*
+             Se voce não quer usar veto no argumento de um método, tu pode usar o "Params" no método que recebe o "vetor"
+                     Exemplo: public static int Sum02(params int[] numbers) definida na class Calculadora
+            O "params" avisa para o compilador que vc quer uma função que possa receber uma quantia variável de parâmetros.
+             */
+            int s4 = Calculadora.Sum02( 2, 3, 4, 4, 7, 8, 5, 8, 7 );
+            Console.WriteLine("A soma de 2 + 3 + 4 + 4 + 7 + 8 é de: " + s4);
         }
     }
 }
