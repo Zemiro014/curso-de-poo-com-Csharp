1b48723b94004a5ec5dc6fd47ac382707075be20
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-21 10:50:52 -0300

    Customizando tipos struct (tipo valor)

---
+++ Course/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    // Para customizar um tipo struct "tipo valor"
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
--- Course/Program.cs
+++ Course/Program.cs
@@ -7,47 +7,12 @@ namespace Course
     {       
         static void Main(string[] args)
         {
-            ContaBancaria conta;
-            Console.Write("Entre o numero da conta: ");
-            int numero = int.Parse(Console.ReadLine());
-
-            Console.Write("Entre o titutal da conta: ");
-            string titular = Console.ReadLine();
-
-            Console.Write("Haverá depósito inicial (s/n)?: ");
-            char respo = char.Parse(Console.ReadLine());
-            if (respo == 's' || respo == 'S')
-            {
-                Console.Write("Entre o valor deposito inicial: ");
-                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                conta = new ContaBancaria(titular, numero, depositoInicial);
-            }
-            else 
-            {
-                conta = new ContaBancaria(titular, numero);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Dados da conta");
-            Console.WriteLine(conta);
-
-            Console.WriteLine();
-            Console.WriteLine("Entre um valor para depósito");
-            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Deposito(quantia);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados da conta atualizados");
-            Console.WriteLine(conta);
-
-            Console.WriteLine();
-            Console.WriteLine("Entre um valor para saque");
-            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            conta.Saque(quantia);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados da conta atualizados");
-            Console.WriteLine(conta);
+            // Um struct precisa de ter os seus campos inicializados ou inicializado usando "new Point()" 
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            p = new Point();
+            Console.WriteLine(p);
         }
     }
 }
