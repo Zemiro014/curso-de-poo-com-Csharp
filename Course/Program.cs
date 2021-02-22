c6ebbd69b672b49ed28c4a1c258568433b4fe893
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-02-22 11:53:17 -0300

    Balanced parentheses

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,51 +1,56 @@
 ﻿using Course.Entities.Enums;
 using Course.Entities;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace Course
 {
     class Program
     {
-        static void Main(string[] args)
+        static string isBalanced(string s) 
         {
-            Console.WriteLine("Enter department's name: ");
-            string deptName = Console.ReadLine();
-            Console.WriteLine("Enter worker data:");
-            Console.Write("Nome: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Level (Junior/MidLevel/Senior): ");
-            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
-            Console.Write("Base salary: ");
-            double basesalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Department dept = new Department(deptName);
-            Worker worker = new Worker(name, level, basesalary, dept);
-            Console.Write("How many contracts to this worker?: ");
-            int n = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= n; i++) 
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++) 
             {
-                Console.WriteLine($"Enter #{i} contract data: ");
-                Console.Write("Date (DD/MM/YYYY): ");
-                DateTime date = DateTime.Parse(Console.ReadLine());
-                Console.Write("Value per hour: ");
-                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-                Console.Write("Duration (hours): ");
-                int hour = int.Parse(Console.ReadLine());
-                HourContract contract = new HourContract(date, valuePerHour, hour);
-                worker.AddContract(contract);
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack.Push(s[i]);
+                }
+                else 
+                {
+                    if (stack.Count == 0)
+                    {
+                        return "NO";
+                    }                    
+                    else 
+                    {
+                        char pop_val = stack.Pop();
+                        if (s[i] == ')' && pop_val != '(') 
+                        {
+                            return "NO";
+                        }
+                        else if (s[i] == '}' && pop_val != '{')
+                        {
+                            return "NO";
+                        }
+                        else if (s[i] == ']' && pop_val != '[')
+                        {
+                            return "NO";
+                        }
+                    }
+                }
             }
 
-            Console.WriteLine();
-            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
-            string monthAndYear = Console.ReadLine();
-            int month = int.Parse(monthAndYear.Substring(0, 2));
-            int year = int.Parse(monthAndYear.Substring(3));
-            Console.WriteLine("Name: "+worker.Name);
-            Console.WriteLine("Department: " + worker.Department.Name);
-            Console.WriteLine("Income for " + monthAndYear + ": " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+            if (stack.Count == 0)
+            {
+                return "YES";
+            }
+            else { return "NO"; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isBalanced("{{[[(())]]}}"));
         }
     }
 }
